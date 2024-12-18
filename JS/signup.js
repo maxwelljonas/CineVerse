1faa4379d7d8426f4a896dc223f1a6a2eaca994e
maxwelljonas <joyonah069@gmail.com>
2024-12-18 11:50:24 +0100

    Made changes to signup page and created a js file for it

---
+++ JS/signup.js
@@ -0,0 +1,5 @@
+
+   document.getElementById("nextBtn").addEventListener("click", function (e) {
+      e.preventDefault(); // Prevent form submission
+      window.location.href = "../pages/subscription.html"; // Navigate to the subscription page
+   });
